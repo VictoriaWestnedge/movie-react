85d9cede2bad7481c28f4318322873d85460ab85
Victoria Westnedge <122117282+VictoriaWestnedge@users.noreply.github.com>
2023-05-02 11:37:17 -0400

    added search functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.css";
-import { Row, Col } from "react-bootstrap";
+import { Row, Col, Form, FormControl } from "react-bootstrap";
 import "./App.css";
 import Banner from "./components/Banner";
 import { FaThumbsUp } from "react-icons/fa";
@@ -24,6 +24,8 @@ export default function App() {
     setPopUpContent([value]);
     setpopuptoggle(!popuptoggle);
   };
+  const apiSearch = "https://api.themoviedb.org/3/search/movie?api_key=be5f56c94025cd4e4b6cbded9f0cab75&language=en-US&page=1&include_adult=false"
+  const [query, setQuery] = useState('')
 
   useEffect(() => {
     const api = async () => {
@@ -40,11 +42,40 @@ export default function App() {
     api();
   }, []);
 
+  const searchMovie = async(e) => {
+    e.preventDefault();
+    console.log("search")
+    try{
+      const url = `https://api.themoviedb.org/3/search/movie?api_key=be5f56c94025cd4e4b6cbded9f0cab75&language=en-US&page=1&include_adult=false&query=${query}`;
+      const res = await fetch(url);
+      const data = await res.json();
+      console.log(data);
+      setResult(data.results);
+    }
+    catch(e) {
+      console.log(e);
+    }
+  }
+
+  const changeHandler=(e) => {
+    setQuery(e.target.value)
+  }
+
   return (
     <div className="App p-5">
       <div className="banner mb-5">
         <Banner />
       </div>
+        <Form className="d-flex justify-content-center mb-5" onSubmit={searchMovie}>
+          <FormControl type="search"
+          placeholder="Search for a movie"
+          aria-label="search"
+          className="form w-25"
+          name="query"
+          value={query} onChange={changeHandler}>
+          </FormControl>
+          <button className="btn btn-secondary ms-2" type="submit">Search</button>
+        </Form>
       <Row>
         {result.map((value) => {
           const truncate = (description) =>
@@ -104,18 +135,24 @@ export default function App() {
                   return (
                     <div className="pop_up_details d-flex flex-row">
                       <div className="image">
-                        <img src={url + content.poster_path} />
+                        <img src={url + content.poster_path} className="pop_up_image"/>
                       </div>
-                      <div className="p-4">
-                        <p>
-                          Name: {content.title} {content.name}
-                        </p>
-                        <p> Type: {content.media_type}</p>
-                        <p> Overview: {content.overview}</p>
-                        <p> Release Date: {content.release_date}</p>
+                      <div className="p-5">
+                        <h2 className="pb-2">
+                          {content.title} {content.name}
+                        </h2>
+                        <h5 className="pb-2"> Type: {content.media_type}</h5>
+                        <h5 className="pb-2"> {content.release_date?.length > 0
+                        ? `Release Date: ${content.release_date}` : "" }</h5>
+                        <h5 className="pb-3">
+                          {" "}
+                          Average Rating:{" "}
+                          {Math.round(content.vote_average * 10)}%
+                        </h5>
+                        <h6> {content.overview}</h6>
                         <div className="pop_up_footer w-100">
                           <button
-                            className="btn btn-dark"
+                            className="btn btn-secondary mt-3"
                             onClick={changeContent}
                           >
                             Close
--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,19 +1,17 @@
+import { Form, FormControl } from "react-bootstrap";
 
 export default function Banner() {
   return (
     <header style={{ paddingLeft: 0 }}>
       <div
-        className='p-5 text-center bg-black'
+        className='p-4 text-center bg-black'
         // style={{ backgroundImage: "url('https://milnersblog.files.wordpress.com/2019/10/star-wars-episode-9-the-rise-of-skywalker-theatre-movie-poster-textless-and-expanded-web-banner.jpg?w=1200')", backgroundRepeat:"no-repeat"}}
       >
         <div className='mask' style={{ backgroundColor: 'rgba(0, 0, 0, 0.6)' }}>
           <div className='d-flex justify-content-center align-items-center h-00'>
             <div className='text-white'>
-              <h1 className='mb-3'>Victoria's Film List</h1>
-              <h4 className='mb-3'>Trending movies and shows today!</h4>
-              <a className='btn btn-outline-light btn-lg' href='#!' role='button'>
-                Call to action
-              </a>
+              <h1 className='mt-5 mb-3'>Victoria's Film List</h1>
+              <h2 className='mb-5'>Trending movies and shows today</h2>
             </div>
           </div>
         </div>
